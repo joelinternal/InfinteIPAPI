483751d20fbd663dd3092e2643cb311838eaeb05
Aldin Philo <aldinphilo@gmail.com>
2025-04-27 18:54:27 +0530

    Revert "Untrack InfiniteIP/Migrations/ folder and update .gitignore"
    This reverts commit b407597d340999b882894f0595589882737ac9f8.

---
+++ InfiniteIP/Migrations/20250417113659_addedsowhours.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace InfiniteIP.Migrations
+{
+    /// <inheritdoc />
+    public partial class addedsowhours : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<decimal>(
+                name: "hours",
+                table: "GmSheet",
+                type: "numeric",
+                nullable: false,
+                defaultValue: 0m);
+
+            migrationBuilder.AddColumn<int>(
+                name: "sow",
+                table: "GmSheet",
+                type: "integer",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "hours",
+                table: "GmSheet");
+
+            migrationBuilder.DropColumn(
+                name: "sow",
+                table: "GmSheet");
+        }
+    }
+}
